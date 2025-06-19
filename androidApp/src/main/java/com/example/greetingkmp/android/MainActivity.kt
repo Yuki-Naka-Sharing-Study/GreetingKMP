6b1f5e49c77ccf5a40fedbad862fee21172f184c
仲優樹 <yu-ki216m@outlook.jp>
2025-06-19 12:40:38 +0900

    Displays the list of strings.

--- androidApp/src/main/java/com/example/greetingkmp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/greetingkmp/android/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.greetingkmp.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.greetingkmp.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    App()
                 }
             }
         }
@@ -27,14 +33,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+@Preview
+fun App() {
+    MaterialTheme {
+        val greeting = remember { Greeting().greet() }
+
+        Column(
+            modifier = Modifier
+                .padding(all = 10.dp)
+                .safeContentPadding()
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            greeting.forEach { greeting ->
+                Text(greeting)
+//                HorizontalDivider()
+            }
+        }
+    }
 }
 
 @Preview
 @Composable
-fun DefaultPreview() {
+fun AppPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        App()
     }
 }
