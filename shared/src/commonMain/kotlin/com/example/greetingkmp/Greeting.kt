3aa706ffb22a3f73aeb8cdf9a604550f4847dd11
仲優樹 <yu-ki216m@outlook.jp>
2025-06-19 11:53:29 +0900

    Imported Random library.

--- shared/src/commonMain/kotlin/com/example/greetingkmp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/greetingkmp/Greeting.kt
@@ -1,9 +1,14 @@
 package com.example.greetingkmp
 
+import kotlin.random.Random
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+
+        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
+
+        return "$firstWord Guess what this is! > ${platform.name.reversed()}!"
     }
 }
